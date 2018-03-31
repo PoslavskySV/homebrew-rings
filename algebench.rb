56927d25e79291baa490e0cf2f92c25d1da47818
Stanislav Poslavsky <stvlpos@mail.ru>
2018-03-31 22:29:39 +0300

    Update algebench.rb

--- algebench.rb
+++ algebench.rb
@@ -3,7 +3,7 @@ class Algebench < Formula
   homepage "https://github.com/PoslavskySV/algebench"
   url "https://github.com/PoslavskySV/algebench/releases/download/v1.0/algebench-1.0.zip"
   version "1.0"
-  sha256 "d6f0188ac7913490f06ed9d01b5521aff48cd10b0712c6134e3281d331e34277"
+  sha256 "b66046689f5d886a9b28026908e4e8456d882bf37f80ec1a5ced6658e6d7537d"
 
   depends_on :java
 
