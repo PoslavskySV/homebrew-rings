d7e140b9a34fef18ef9b3a3f03bceb3281ac2dd1
Stanislav Poslavsky <stvlpos@mail.ru>
2018-04-11 21:49:16 +0300

    Update algebench.rb

--- algebench.rb
+++ algebench.rb
@@ -1,8 +1,8 @@
 class Algebench < Formula
   # desc "algebench: a tool for benchmarking libraries/systems for doing commutative algebra."
   homepage "https://github.com/PoslavskySV/algebench"
-  url "https://github.com/PoslavskySV/algebench/releases/download/v1.0/algebench-1.0.zip"
-  version "1.0"
+  url "https://github.com/PoslavskySV/algebench/releases/download/v1.1/algebench-1.1.zip"
+  version "1.1"
   sha256 "f6b7fa048531e874b5fc4fbab647cfb1d801fd3fb58283aab3b3282a278aaf6c"
 
   depends_on :java
