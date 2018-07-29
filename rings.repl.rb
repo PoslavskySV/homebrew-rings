59f5f0065edbe48157a29570ba8237ba8b9c4e27
Stanislav Poslavsky <stvlpos@mail.ru>
2018-07-29 03:48:00 +0300

    Update rings.repl.rb

--- rings.repl.rb
+++ rings.repl.rb
@@ -2,7 +2,7 @@ class RingsRepl < Formula
   desc "Rings: efficient Java/Scala library for polynomial rings"
   homepage "http://ringsalgebra.io/"
   url "https://raw.githubusercontent.com/PoslavskySV/rings/v2.5.1/rings.repl/rings.repl", :using => :nounzip
-  sha256 "3dd8cdc57d4a1a6eae7e2e99e14af495a76012bd0da398720ccc6976117d7372"
+  sha256 "ec53ca1f970addcce0aae043441964bf9b5011815aa565e353472733671d3a25"
 
   depends_on "ammonite-repl"
 
